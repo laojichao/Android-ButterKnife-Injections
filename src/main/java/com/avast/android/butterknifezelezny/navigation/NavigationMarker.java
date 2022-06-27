c71b9ce9071a8665095cde5693a1a31da52851c0
aotem <49890132@qq.com>
2022-06-28 00:33:33 +0800

    Initial commit

---
+++ src/main/java/com/avast/android/butterknifezelezny/navigation/NavigationMarker.java
@@ -0,0 +1,58 @@
+package com.avast.android.butterknifezelezny.navigation;
+
+import com.google.common.collect.Lists;
+import com.intellij.codeInsight.daemon.DefaultGutterIconNavigationHandler;
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMember;
+import com.intellij.psi.PsiTypeElement;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+import static com.intellij.codeHighlighting.Pass.UPDATE_ALL;
+import static com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.LEFT;
+/**
+ * @author lao
+ * @version 1.0.0
+ * @description 导航图标
+ * @since 2022/6/27
+ */
+class NavigationMarker extends LineMarkerInfo<PsiElement> {
+
+
+    private static final Icon ICON = IconLoader.getIcon("/icons/butterknife-zelezny.png", null);
+
+    private NavigationMarker(@NotNull final PsiElement source, @NotNull final PsiMember destination,
+                             @NotNull final TextRange textRange) {
+
+        super(source, textRange, ICON, null, new DefaultGutterIconNavigationHandler<PsiElement>(Lists.newArrayList(destination), ""), LEFT, null);
+//        super(source, textRange, ICON, UPDATE_ALL, null,
+//                new DefaultGutterIconNavigationHandler<PsiElement>(Lists.newArrayList(destination), ""), LEFT);
+    }
+
+    static class Builder {
+        private PsiElement source;
+        private PsiMember destination;
+
+        Builder from(@NotNull PsiElement source) {
+            this.source = source;
+            return this;
+        }
+
+        Builder to(@NotNull PsiMember destination) {
+            this.destination = destination;
+            return this;
+        }
+
+        @NotNull
+        NavigationMarker build() {
+            final PsiTypeElement typeElement = source instanceof PsiField ? ((PsiField) source).getTypeElement() : null;
+            final TextRange textRange = typeElement != null ? typeElement.getTextRange() : source.getTextRange();
+            return new NavigationMarker(source, destination, textRange);
+        }
+    }
+}
